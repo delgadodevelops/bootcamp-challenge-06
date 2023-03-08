a78367d16b9993b7b1875d73087bd8f2848c8770
ddelgado25 <daviddelgado2515@gmail.com>
2023-03-08 17:17:13 -0500

    Added footer element to html for Previously searched cities

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,3 +10,4 @@ function getApi() {
     var requestFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
     var requestToday = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
 }
+
