5444b82e42611d8bfd55a11f4d88fd68e1bb26d0
ddelgado25 <daviddelgado2515@gmail.com>
2023-03-08 16:49:50 -0500

    Created variabales and added API function to get today forcast and 5 day fore cast

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,6 +5,7 @@ var clicked = false;
 var searchHistory = [];
 var currentCity = document.querySelector("#current-city");
 
+// Get API Function
 function getApi() {
     var requestFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
     var requestToday = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=03e2b0141cd0f7d9d15a27103279bb3e&units=metric";
